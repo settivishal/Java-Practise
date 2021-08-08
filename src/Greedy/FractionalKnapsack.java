82c5ae3939f2825aa392aeee133badb412b39b20
vishalsetti <vishalsetti2000@gmail.com>
2021-08-08 21:38:43 +0530

    added TCS questions

--- src/Greedy/FractionalKnapsack.java
+++ src/Greedy/FractionalKnapsack.java
@@ -1,5 +1,17 @@
 package Greedy;
 
+//! Given weights and values of n items, we need to put these items in a knapsack of capacity W to get the maximum total value in the knapsack.
+
+//* Input: 
+//* Items as (value, weight) pairs 
+//* arr[] = {{60, 10}, {100, 20}, {120, 30}} 
+//* Knapsack Capacity, W = 50; 
+
+//* Output: 
+//* Maximum possible value = 240 
+//* by taking items of weight 10 and 20 kg and 2/3 fraction 
+//* of 30 kg. Hence total price will be 60+100+(2/3)(120) = 240
+
 // Java program to solve fractional Knapsack Problem
 import java.util.Arrays;
 import java.util.Comparator;
